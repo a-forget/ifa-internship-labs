0827f42ab8ea74fbd63e836b803626b3897daa5a
a-forget <aforget@disroot.org>
2025-09-12 17:25:45 -0400

    Worked on capstone project; restructured and renamed some files

---
+++ capstone-project/javascript/script.js
@@ -0,0 +1,14 @@
+// From
+// https://lucide.dev/guide/packages/lucide
+
+// Recommended way, to include only the icons you need.
+lucide.createIcons();
+
+// import { createIcons, Menu} from 'lucide';
+
+// createIcons({
+//   icons: {
+//     Menu
+//   }
+// });
+
